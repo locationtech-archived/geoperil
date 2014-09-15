cae2b2fd869015aaee31e8580811111aeadedb44
Sven Reißland <sven.reissland@gfz-potsdam.de>
2014-09-15 15:36:55 +0000

    updating of stations now supported

--- slm2tc/slm2tc.py
+++ slm2tc/slm2tc.py
@@ -12,8 +12,12 @@ IOCSLMSRV = "http://www.ioc-sealevelmonitoring.org/service.php"
 TIMEOUT = 300
 
 configfile = "config.json"
+cmd = "retrievedata"
 if len(sys.argv) > 1:
     configfile = sys.argv[1]
+    if len(sys.argv) > 2:
+        cmd = sys.argv[2]
+
 
 def getdataforstation(args):
     config, s = args
@@ -56,52 +60,106 @@ def getdataforstation(args):
     sys.stderr.flush()
 
 
-if os.path.exists(configfile):
-    print("Loading configuration file %s." % configfile)
-    cf = open(configfile,"r")
-    config = json.load(cf)
+def updatestationmetadata(config):
+    n = 0
+    for sname,s in config["stations"].items():
+        n += 1
+        print("Updating station metadata %d/%d: %s..." % (n,len(config["stations"]),s["name"]), end="")
+        sdetail = requests.get(IOCSLMSRV, params={"query":"station", "code":s["slmcode"]}, timeout=TIMEOUT).json()
+        news = {
+            "name":sname, 
+            "slmcode":s["slmcode"], 
+            "lon":float(sdetail["Lon"]), 
+            "lat":float(sdetail["Lat"]),
+            }
+        if "sensor" in s:
+            news["sensor"] = s["sensor"]
+        for a in ['Location', 'units', 'type', 'countryname', 'UTCOffset', 'country', 'offset']:
+            v = sdetail.get(a,None)
+            if v is not None:
+                news[a] = v
+        params = {
+            "apiver":"1", 
+            "inst":config["inst"], 
+            "secret":config["secret"],
+            "station":json.dumps(news),
+            }
+        sfeed = requests.post(config["feedurl"]+"/feedstation", data=params, timeout=TIMEOUT).json()
+        print(sfeed)
+        news["lasttimestamp"] = s["lasttimestamp"]
+        config["stations"][sname] = news
+    print("Updating station metadata done.")
+
+
+def loadconfig(configfile):
+    if os.path.exists(configfile):
+        print("Loading configuration file %s." % configfile)
+        cf = open(configfile,"r")
+        config = json.load(cf)
+        cf.close()
+        return config
+    else:
+        print("Configuration file %s does not exist." % configfile)
+        sys.exit()
+
+
+def saveconfig(configfile,config):
+    cf = open(configfile,"w")
+    json.dump(config, cf, indent=4)
     cf.close()
-    if config["updatestations"]:
-        n = 0
-        for sname,s in config["stations"].items():
-            n += 1
-            print("Updating stations %d/%d: %s..." % (n,len(config["stations"]),s["name"]), end="")
-            sdetail = requests.get(IOCSLMSRV, params={"query":"station", "code":s["slmcode"]}, timeout=TIMEOUT).json()
-            news = {
-                "name":sname, 
-                "slmcode":s["slmcode"], 
-                "lon":float(sdetail["Lon"]), 
-                "lat":float(sdetail["Lat"]),
-                }
-            if "sensor" in s:
-                news["sensor"] = s["sensor"]
-            for a in ['Location', 'units', 'type', 'countryname', 'UTCOffset', 'country', 'offset']:
-                v = sdetail.get(a,None)
-                if v is not None:
-                    news[a] = v
-            params = {
-                "apiver":"1", 
-                "inst":config["inst"], 
-                "secret":config["secret"],
-                "station":json.dumps(news),
-                }
-            sfeed = requests.post(config["feedurl"]+"/feedstation", data=params, timeout=TIMEOUT).json()
-            print(sfeed)
-            news["lasttimestamp"] = s["lasttimestamp"]
-            config["stations"][sname] = news
-        print("Updating stations done.")
+
+
+if cmd=="retrievedata":
+    config = loadconfig(configfile)
     stations = []
     for s in config["stations"].values():
         stations.append((config,s))
     tp = ThreadPool(config["threads"])
     tp.map(getdataforstation,stations)
-        
-    cf = open(configfile,"w")
-    json.dump(config, cf, indent=4)
-    cf.close()
-else:
-    print("Configuration file %s does not exist, creating new one." % configfile)
-    config = {"feedurl":"", "inst":"", "secret":"", "updatestations":True, "threads":4, "stations":{}}
+    saveconfig(configfile,config)
+elif cmd=="updatemetadata":
+    config = loadconfig(configfile)
+    updatestationmetadata(config)
+    saveconfig(configfile,config)
+elif cmd=="updatestations":
+    config = loadconfig(configfile)
+    
+    newstations = []
+    oldstations = list(config["stations"].keys())
+
+    stationlist = requests.get(IOCSLMSRV, params={"query":"stationlist"}, timeout=TIMEOUT).json()
+    for s in stationlist:
+        stationname = s["Code"]
+        if "sensor" in s and s["sensor"] is not None:
+            stationname += "_" + s["sensor"]
+        if stationname in oldstations:
+            oldstations.remove(stationname)
+        elif stationname in config["stations"]:
+            print("Duplicate entry for %s." % stationname)
+        else:
+            newstations.append(stationname)
+            config["stations"][stationname] = {}
+            config["stations"][stationname]["name"] = stationname
+            config["stations"][stationname]["lasttimestamp"] = None
+        config["stations"][stationname]["slmcode"] = s["Code"]
+        config["stations"][stationname]["lon"] = s["Lon"]
+        config["stations"][stationname]["lat"] = s["Lat"]
+        for a in ['Location', 'units', 'type', 'countryname', 'UTCOffset', 'country', 'offset', 'sensor']:
+            v = s.get(a,None)
+            if v is not None:
+                config["stations"][stationname][a] = v
+    if len(newstations)>0:
+        print("New stations in SLM (%d):" % len(newstations))
+        for s in newstations:
+            print("    %s" % s)
+    if len(oldstations)>0:
+        print("Old stations no longer provided by SLM (%d):" % len(oldstations))
+        for s in oldstations:
+            print("    %s" % s)
+    saveconfig(configfile,config)
+elif cmd=="genconfig":
+    print("Creating configuration file %s..." % configfile)
+    config = {"feedurl":"", "inst":"", "secret":"", "threads":4, "stations":{}}
     stationlist = requests.get(IOCSLMSRV, params={"query":"stationlist"}, timeout=TIMEOUT).json()
     for s in stationlist:
         stationname = s["Code"]
@@ -117,6 +175,7 @@ else:
             v = s.get(a,None)
             if v is not None:
                 config["stations"][stationname][a] = v
-    cf = open(configfile,"w")
-    json.dump(config, cf, indent=4)
-    cf.close()
+
+    saveconfig(configfile,config)
+else:
+    print("Command %s not known." % cmd)
