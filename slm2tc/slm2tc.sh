837719550047383ffe93833dc40d13aebb7d460a
Sven Reißland <sven.reissland@gfz-potsdam.de>
2014-12-04 15:38:50 +0000

    now using v2 as default

--- slm2tc/slm2tc.sh
+++ slm2tc/slm2tc.sh
@@ -6,6 +6,6 @@ if [ ! -e running.lock ] ; then
     if [ `du -s slm2tc.log | cut -f1` -gt 100000 ] ; then
         mv slm2tc.log slm2tc.log.old
         fi
-    ./slm2tc.py $* >> slm2tc.log 2>&1
+    ./slm2tc2.py $* >> slm2tc.log 2>&1
     rm -f running.lock
     fi
