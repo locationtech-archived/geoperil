b1518e82edad16db2c9f86b68c9c3cd410355dea
Johannes Spazier <johannes.spazier@gfz-potsdam.de>
2015-06-24 18:27:32 +0000

    Added missing file.

---
+++ server/www/geofon.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>GEOFON Event</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<base target="_blank"/>
+<style>
+	.sections {
+		min-width: 500px;
+		max-width: 600px;
+		margin: auto;
+		padding-top: 0px;
+		text-align: center;
+	}
+	.sections > h3 {
+		background-color: #00589C;
+		color: white;
+ 		padding: 0.5em;
+		font-weight: bold;
+		width: calc(100%-1em);
+	}
+	.sections > h3 a {
+		color: white;
+		text-align: right;
+ 		font-size: 14px;
+		float: right;
+	}
+</style>
+</head>
+<body>
+
+<div class="sections">
+<?php
+	$evtid = htmlspecialchars($_GET["id"]);
+	$file = 'http://geofon.gfz-potsdam.de/eqinfo/event.php?id='. $evtid;
+	$geofon = file_get_contents($file);
+	$geofon = str_replace("='/data/", "='http://geofon.gfz-potsdam.de/data/", $geofon);
+	$geofon = str_replace("=\"/eqinfo/", "=\"http://geofon.gfz-potsdam.de/eqinfo/", $geofon);
+	$geofon = str_replace("<a href='list.php'>Back to the earthquake list</a><br />", "", $geofon);
+	//$geofon = str_replace("<a href", "<a "
+	preg_match('/<div id="GEcontent">(.*?)<\/div> <!-- GEcontent -->/s', $geofon, $div);
+?>
+<h3 id="geofon">GEOFON
+	<?php if( ! empty( $div[1] ) ) : ?>
+		<a href="<?php echo $file;?>">Go to source</a>
+	<?php endif;?>
+</h3>
+<div class="sec">
+<?php
+	if( empty( $div[1] ) )
+		echo "<b>No data available.</b>";
+	echo $div[1];
+?>
+</div>
+</div>
+
+</body>
+</html>
