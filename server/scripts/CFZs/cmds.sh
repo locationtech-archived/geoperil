05c7845429ea992589e64d46f69a093e0d94503d
Johannes Spazier <johannes.spazier@gfz-potsdam.de>
2016-01-26 12:38:21 +0000

    Added a python script to insert CFZs and correspondig TSPs into the database.

---
+++ server/scripts/CFZs/cmds.sh
@@ -0,0 +1,1 @@
+python3 ./insert.py CFZs_caribic.gmt TSPs_caribic.csv 
---
+++ server/scripts/CFZs/insert.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+import sys
+import re
+import datetime
+from pymongo import MongoReplicaSetClient
+
+client = MongoReplicaSetClient("mongodb://tcnode1,tcnode2,tcnode3/?replicaSet=tcmongors0" ,w="majority")
+db = client['trideccloud']
+
+def import_cfz(self, fcfzs, ftsps):
+    f = open(fcfzs, 'r')
+    content = f.read()
+    cfz = re.findall('^>.*\n([^>]*\n)*', content, re.M)
+    objs = []
+    now = datetime.datetime.utcnow()
+    maxid = list(db["cfcz"].find().sort("FID_IO_DIS", pymongo.DESCENDING).limit(1))[0]["FID_IO_DIS"]
+    for poly in cfz:
+        meta = re.search('^\s*#\s*@D(?P<country>[^|]*)\|(?P<state>[^|]*)\|(?P<id>[1-9][0-9]*).*$', poly, re.M)
+        points = [(float(x), float(y)) for (x,y) in re.findall('(-?\d*\.\d*) (-?\d*\.\d*)', poly)]
+        if re.search('^\s*#\s*@P', poly, re.M) is not None:
+            obj = {
+                "_COORDS_": [points],
+                "COUNTRY": meta.group("country").replace('"',''),
+                "STATE_PROV": meta.group("state").replace('"',''),
+                "FID_IO_DIS": maxid + int(meta.group("id")),
+                "date": now
+            }
+            objs.append( obj )
+        elif re.search('^\s*#\s*@H', poly, re.M) is not None:
+            objs[-1]["_COORDS_"].append(points)
+        else:
+            raise ValueError('Unknown polygon format.')
+    f.close()
+    db["cfcz"].insert(objs)
+    with open(ftsps, 'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+        tsps = []
+        for row in csvreader:
+            tsps.append( {"lon_sea": float(row[0]), "lat_sea": float(row[1]), "FID_IO_DIS": maxid + int(row[4]), "date": now} )
+        db["tsps"].insert( tsps )
+
+
+if len(sys.argv) < 3:
+    return print("To few arguments given!")
+
+client = MongoReplicaSetClient("mongodb://tcnode1,tcnode2,tcnode3/?replicaSet=tcmongors0" ,w="majority")
+db = client['trideccloud']
+
+import_cfz(sys.argv[1], sys.argv[2])
+
+client.close()
+
