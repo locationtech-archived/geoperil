c36a4b390613b7e7d3fa3870e7b56d0d5f4004b4
Matthias Ruester <matthias.ruester@gfz-potsdam.de>
2019-07-26 12:08:36 +0200

    Add example data for the 'stations' collection

--- server/mongodb-data/import-example-data.sh
+++ server/mongodb-data/import-example-data.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-for i in users settings institutions events; do
+for i in users settings institutions events stations; do
     echo "Importing collection: $i"
     mongoimport --mode=upsert --host "$MONGO_HOST" --db "$MONGO_DB" --collection $i --type json --file /tmp/example-data/$i.json --jsonArray
 done
