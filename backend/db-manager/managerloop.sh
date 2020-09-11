410ca242da75c91cfdc75dd55d917962af820896
Matthias Ruester <matthias.ruester@gfz-potsdam.de>
2020-09-11 15:37:14 +0200

    Backend: Improve loop scripts

--- backend/db-manager/managerloop.sh
+++ backend/db-manager/managerloop.sh
@@ -4,17 +4,21 @@ GEOFON_LAST300_URL="${GEOFON_LIST_URL}?fmt=geojson&nmax=300"
 export GEOFON_OUTPUT="geofon-last300.geojson"
 
 cd `dirname $0`
+touch "db-manager.log"
 
 trap "rm -f running.lock" EXIT
 
 while true; do
     if [ ! -e running.lock ]; then
         echo $$ > running.lock
+        if [ "$(du -s db-manager.log | cut -f1)" -gt 100000 ] ; then
+            mv "db-manager.log" "db-manager.log.old"
+            touch "db-manager.log"
+        fi
         rm "${GEOFON_OUTPUT}" || true
         wget "${GEOFON_LAST300_URL}" -O "${GEOFON_OUTPUT}"
-        python3 manager.py 2>&1 | tee -a "db-manager.log"
-        rm -f running.lock
+        python3 manager.py 2>&1 | tee -a "db-manager.log" && rm -f running.lock &
     fi
 
-    sleep 120
+    sleep 20
 done
--- backend/eventfeeder/feederloop.sh
+++ backend/eventfeeder/feederloop.sh
@@ -3,8 +3,16 @@
 PYFILE="eventfeeder.py"
 LOGFILE="eventfeeder.log"
 
+cd `dirname $0`
+
+trap "rm -f running.lock" EXIT
+
 while true; do
-    echo "Started feeder: $(date)" >> "${LOGFILE}"
-    python3 "${PYFILE}" "${FEEDERSRV_URL}" 2>&1 | tee -a "${LOGFILE}"
+    if [ ! -e running.lock ]; then
+        echo $$ > running.lock
+        echo "Started feeder: $(date)" >> "${LOGFILE}"
+        python3 "${PYFILE}" "${FEEDERSRV_URL}" 2>&1 | tee -a "${LOGFILE}" && rm -f running.lock &
+    fi
+
     sleep 1800 # 30 minutes
 done
